8bd05fc012fdafc111d46c793ec8737a14e0a6b4
ellxsm <ellis.oliveirab@gmail.com>
2024-01-14 01:31:30 -0300

    Calculadora de Partidas Rankeadas

---
+++ calculadora-partidas-rankeadas/index.js
@@ -0,0 +1,33 @@
+//Projeto Calculadora de Partidas Rankeadas
+
+//Variáveis
+let nivelHeroi = "";
+let resultado = calcularSaldo(150, 20);
+
+//Função
+function calcularSaldo(qtdVitorias, qtdDerrotas) {
+  return qtdVitorias - qtdDerrotas;
+}
+
+//Estrutura de Decisão
+if (resultado <= 10) {
+  nivelHeroi = "Ferro";
+} else if (resultado >= 11 && resultado <= 20) {
+  nivelHeroi = "Bronze";
+} else if (resultado >= 21 && resultado <= 50) {
+  nivelHeroi = "Prata";
+} else if (resultado >= 51 && resultado <= 80) {
+  nivelHeroi = "Ouro";
+} else if (resultado >= 81 && resultado <= 90) {
+  nivelHeroi = "Diamante";
+} else if (resultado >= 91 && resultado <= 100) {
+  nivelHeroi = "Lendário";
+} else {
+  nivelHeroi = "Imortal";
+}
+
+console.log(
+  "O Herói tem de saldo " + resultado,
+  "e está no nível " + nivelHeroi,
+  "!"
+);
